7b3d38ad87a5f2be8ac67c3ef1409e06aebb0025
Khalil <khalilundo12@gmail.com>
2025-05-03 18:45:15 +0800

    added json-server api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const POKE_API_BASE = 'https://pokeapi.co/api/v2';
-const JSON_SERVER_BASE = 'http://localhost:3001';
+const JSON_SERVER_BASE = 'https://pokemon-jsonserver.onrender.com';
 
 // PokeAPI services
 export const getPokemonList = async (limit = 20, offset = 0) => {
